9803ff511783c5ca32a1f6dc45bb3b9c979099f0
Nagato <transcendentalliouville@gmail.com>
2026-04-24 00:36:56 +0900

    modified link

--- satoshin-des.github.io/app/components/Header.vue
+++ satoshin-des.github.io/app/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-        <NuxtLink to="/index"><img src="/satoshin_profile.jpg" alt="さとしんでぇす" width="50"></NuxtLink>
+        <NuxtLink to="/"><img src="/satoshin_profile.jpg" alt="さとしんでぇす" width="50"></NuxtLink>
         <NuxtLink to="/portfolio">ポートフォリオ</NuxtLink>
         <!--<NuxtLink to="~/math">数学のこと</NuxtLink>
         <NuxtLink to="~/music">音楽のこと</NuxtLink>
