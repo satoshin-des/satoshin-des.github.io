822776ca692fe5a38b4593f4fe883434e2c0f620
Nagato <transcendentalliouville@gmail.com>
2026-04-23 19:13:21 +0900

    Start to implement with Vue.js

---
+++ satoshin-des.github.io/app/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer>
+        <small>
+            さとしん 2024- 2026 All rights reserved
+        </small>
+    </footer>
+</template>
---
+++ satoshin-des.github.io/app/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Footer from '~/components/footer.vue';
+</script>
+
+<template>
+    <h1>さとしんのHP（工事中）</h1>
+    <Footer></Footer>
+</template>
