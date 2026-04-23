d34696c76a7a5a33de96c0bae2878188ba7065a5
Nagato <transcendentalliouville@gmail.com>
2026-04-23 22:33:40 +0900

    implemented index.vue

---
+++ satoshin-des.github.io/app/components/Section.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 style="color:#00ffcc; text-align:center;">
+        <slot />
+    </h1>
+</template>
--- satoshin-des.github.io/app/pages/index.vue
+++ satoshin-des.github.io/app/pages/index.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
 import Footer from '~/components/Footer.vue';
+import Section from '~/components/Section.vue';
 </script>
 
 <template>
     <h1>さとしんのHP（工事中）</h1>
     <p><b>さとしん</b>とは、東京都内の大学院に通うただの大学院生である。</p>
     <p>このホームページでは、私の活動について書いていきます。</p>
+    <Section>Bioglaphy</Section>
+    <table>
+        <tr>
+            <th>ぎりぎり平成</th>
+            <th>立教大学理学部数学科へ入学</th>
+        </tr>
+        <tr>
+            <th>令和になってから</th>
+            <th>優秀論文賞とか取った</th>
+        </tr>
+        <tr>
+            <th>令和4年</th>
+            <th>無事大学を卒業</th>
+        </tr>
+        <tr>
+            <th>同年</th>
+            <th>立教大学大学院へ入学</th>
+        </tr>
+        <tr>
+            <th>令和7年</th>
+            <th>博士課程に進学</th>
+        </tr>
+    </table>
     <Footer></Footer>
 </template>
