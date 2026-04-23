79b820ae3e13edd8f3681a5a844b2932e7d64250
Nagato <transcendentalliouville@gmail.com>
2026-04-24 00:24:28 +0900

    commented-out unused sentences

--- satoshin-des.github.io/app/components/Header.vue
+++ satoshin-des.github.io/app/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
     <header>
-        <NuxtLink to="~/index"><img src="/satoshin_profile.jpg" alt="さとしんでぇす" width="50"></NuxtLink>
-        <NuxtLink to="~/portfolio">ポートフォリオ</NuxtLink>
-        <NuxtLink to="~/math">数学のこと</NuxtLink>
+        <NuxtLink to="/index"><img src="/satoshin_profile.jpg" alt="さとしんでぇす" width="50"></NuxtLink>
+        <NuxtLink to="/portfolio">ポートフォリオ</NuxtLink>
+        <!--<NuxtLink to="~/math">数学のこと</NuxtLink>
         <NuxtLink to="~/music">音楽のこと</NuxtLink>
         <NuxtLink to="~/program">プログラミングのこと</NuxtLink>
         <NuxtLink to="~/book">読書のこと</NuxtLink>
-        <NuxtLink to="~/other_home">その他色々</NuxtLink>
+        <NuxtLink to="~/other_home">その他色々</NuxtLink>-->
     </header>
 </template>
