ea7a8c3a8eb344a727e98db1428e69f2b960992f
Nagato <transcendentalliouville@gmail.com>
2026-04-23 19:18:31 +0900

    Updated index.vue

--- satoshin-des.github.io/app/pages/index.vue
+++ satoshin-des.github.io/app/pages/index.vue
@@ -4,5 +4,7 @@ import Footer from '~/components/footer.vue';
 
 <template>
     <h1>さとしんのHP（工事中）</h1>
+    <p><b>さとしん</b>とは、東京都内の大学院に通うただの大学院生である。</p>
+    <p>このホームページでは、私の活動について書いていきます。</p>
     <Footer></Footer>
 </template>
