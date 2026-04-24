1831c46dee858d7b88d220e5faab614551ac26e8
Nagato <transcendentalliouville@gmail.com>
2026-04-24 18:29:40 +0900

    testly updated math.vue

--- satoshin-des.github.io/app/pages/math.vue
+++ satoshin-des.github.io/app/pages/math.vue
@@ -3,11 +3,12 @@ import VueMathjax from 'vue-mathjax'
 
 import Header from '~/components/Header.vue';
 import Footer from '~/components/Footer.vue';
-import Section from '~/components/Section.vue';
+
+const formula = '$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$';
 </script>
 
 <template>
     <Header />
-    <VueMathjax>f</VueMathjax>
+    <vue-mathjax :formula="formula"></vue-mathjax>
     <Footer />
 </template>
