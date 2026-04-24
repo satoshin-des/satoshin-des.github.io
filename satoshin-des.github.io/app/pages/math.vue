9913854ce98575da3493201058b8605cfca328d6
Nagato <transcendentalliouville@gmail.com>
2026-04-24 18:18:52 +0900

    testly add math

---
+++ satoshin-des.github.io/app/pages/math.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import VueMathjax from 'vue-mathjax'
+
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
+import Section from '~/components/Section.vue';
+</script>
+
+<template>
+    <Header />
+    <VueMathjax>f</VueMathjax>
+    <Footer />
+</template>
