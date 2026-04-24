b8ad4e0f5339712d3eedafa6491e2373a1018c70
Nagato <transcendentalliouville@gmail.com>
2026-04-24 18:42:43 +0900

    changed

--- satoshin-des.github.io/app/pages/math.vue
+++ satoshin-des.github.io/app/pages/math.vue
@@ -6,7 +6,7 @@ import VueMathjax from 'vue-mathjax-next'
 import Header from '~/components/Header.vue';
 import Footer from '~/components/Footer.vue';
 
-const formula = ref('$$x^2 + y^2 = z^2$$');
+const formula = '$$x^2 + y^2 = z^2$$';
 </script>
 
 <template>
