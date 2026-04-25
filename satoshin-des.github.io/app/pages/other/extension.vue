e6368e0158e63b7bc657a6aec446c515bca4cdfd
Nagato <transcendentalliouville@gmail.com>
2026-04-25 13:42:06 +0900

    added import

--- satoshin-des.github.io/app/pages/other/extension.vue
+++ satoshin-des.github.io/app/pages/other/extension.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+import { VueGoodTable } from 'vue-good-table-next';
+import 'vue-good-table-next/dist/vue-good-table-next.css';
+
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
+
 const columns = ref(
     [
         {
